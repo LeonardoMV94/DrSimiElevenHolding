c951dc1e0c8fcb3ccc6878ad9b0d0cea6c46f018
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2024-11-08 22:36:49 -0300

    server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const http =  require('node:http')
+const path = require('node:path')
+const fs = require('node:fs')
+
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+
+    if(req.url == '/'){
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+
+    } else if(req.url === '/contacto' && req.method == 'GET') {
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+
+    } else if(req.url === '/contacto' && req.method == 'POST'){
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        
+    }else {
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        res.send('Pagina')
+    }
+
+})
+
+server.listen(PORT, () => {
+    console.log(`Server listening in http://localhost:${PORT}/contacto`)
+})
